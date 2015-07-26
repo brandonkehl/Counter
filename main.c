3c046c1f3c236d076d139cf6ed4caef9aa011deb
brandonkehl <kehl.brandon@gmail.com>
2015-07-25 23:38:21 -0500

    Added all code

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  loops
+//
+//  Created by brandon kehl on 6/14/15.
+//  Copyright (c) 2015 bignerdranch. All rights reserved.
+//
+
+#import <stdio.h>
+#import <readline/readline.h>
+#include <stdlib.h>
+int main(int argc, const char * argv[] ){
+    printf("Where shall I start counting?  \n",NULL);
+    for(int i = atoi(readline(NULL));i>0;i -=3){
+     printf("%d\n",i);
+    if(i%5==0){
+        printf("Found One!");
+        
+    }
+  }
+}
