f46dc0f3545f967785ffa27598e7693e3f12d822
brandon kehl <kehl.brandon@gmail.com>
2015-10-06 12:04:08 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 //  loops
 //
 //  Created by brandon kehl on 6/14/15.
-//  Copyright (c) 2015 bignerdranch. All rights reserved.
+//  Copyright (c) 2015 brandonkehl. All rights reserved.
 //
 
 #import <stdio.h>
